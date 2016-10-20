cccac212f50990be2949dfb26c4e3bf622527ba0
Navjot Singh <navjotsingh9633@gmail.com>
2016-10-20 18:19:44 +0530

    placed all website data in database and placing on views

--- database/itclub_db.sql
+++ database/itclub_db.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Oct 19, 2016 at 06:33 AM
+-- Generation Time: Oct 20, 2016 at 12:47 PM
 -- Server version: 5.6.12-log
 -- PHP Version: 5.4.16
 
@@ -21,6 +21,69 @@ SET time_zone = "+00:00";
 --
 CREATE DATABASE IF NOT EXISTS `itclub_db` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
 USE `itclub_db`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `aboutclub_table`
+--
+
+CREATE TABLE IF NOT EXISTS `aboutclub_table` (
+  `heading` varchar(50) NOT NULL,
+  `data1` varchar(100) NOT NULL,
+  `data2` varchar(100) NOT NULL,
+  `data3` varchar(100) NOT NULL,
+  `data4` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `aboutclub_table`
+--
+
+INSERT INTO `aboutclub_table` (`heading`, `data1`, `data2`, `data3`, `data4`) VALUES
+('IT CLUB HOW IT WORKS', ' To improve manpower skill through technical Training', ' Decisions on New technology and problems through Seminars', ' To Aware New technology and solves problems through Webinars', ''),
+('IT CLUB FOR WHOM', 'Information Technology Students.', 'Information Technology Professional and Practitioner', 'Information Technology Companies', 'Information Technology Companies');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `activities_table`
+--
+
+CREATE TABLE IF NOT EXISTS `activities_table` (
+  `heading` varchar(20) NOT NULL,
+  `data1` varchar(700) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `activities_table`
+--
+
+INSERT INTO `activities_table` (`heading`, `data1`) VALUES
+('Training', 'Training helps your business run better. It is essential for knowledge transfer. Through this we can give improvements in confidence, capability and competence.'),
+('Seminars', 'In our seminars we share personal experiences regarding it, which provide a great deal in your practical.'),
+('Conferences', 'At the conferences you can discuss any thing regarding information technology and your business');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `home_table`
+--
+
+CREATE TABLE IF NOT EXISTS `home_table` (
+  `heading` varchar(30) NOT NULL,
+  `data1` varchar(700) NOT NULL,
+  `data2` varchar(50) NOT NULL,
+  `data3` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `home_table`
+--
+
+INSERT INTO `home_table` (`heading`, `data1`, `data2`, `data3`) VALUES
+('WHO WE ARE', 'I.T Club India® seeks to Advance the IT Profession through Professional Development & Awareness, Support of IT Education, and Government Policies on IT that improve society as a whole. I.T. Club India® represents Professionals, Companies and Organizations involved in consulting and providing products and services pertaining to Information Technology sector. The primary goal of the I.T. Club India is to promote management practices that will ensure the confidentiality, integrity and availability of information resources.', '', ''),
+('OUR AIMS & OBJECTIVES', 'Technology Awareness', ' Business Engagement and Emplementation', 'Cyber Law & Govt. Policy Awareness');
 
 -- --------------------------------------------------------
 
